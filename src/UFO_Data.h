b45a3f4c6aa821d30626a8ad47507d9c56c46ff7
Alan S. <askrypek@ufl.edu>
2024-11-28 18:20:52 -0500

    Created UFO_Data header which is a class that stores all attributes of UFO sighting. Also created main file that reads csv file and stores in a vector (will be a hash table)

---
+++ src/UFO_Data.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class UFO {
+public:
+
+    string city;
+    string state;
+    string country;
+
+    string shape;
+    string duration;
+    string description;
+
+    double latitude;
+    double longitude;
+
+    string year;
+    string month;
+    string day;
+    string hour;
+    string minute;
+
+    string year_doc;
+    string month_doc;
+    string day_doc;
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "UFO_Data.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    ifstream file("ufo_sightings.csv");
+
+    // use for testing csv reading
+//    string line;
+//    bool isHeader = true;
+//    int lineCount = 0;
+//
+//    while (getline(file, line)) {
+//        if (isHeader) {
+//            isHeader = false;
+//            continue;
+//        }
+//
+//        lineCount++;
+//        if (lineCount >= 50) {
+//            break;
+//        }
+//    }
+//
+//    file.close();
+
+
+    vector<UFO> ufoList;
+    string line;
+    bool isHeader = true;
+
+    while (getline(file, line)) {
+
+        // Skip the header line
+        if (isHeader) {
+            isHeader = false;
+            continue;
+        }
+
+        stringstream ss(line);
+        UFO ufo;
+        string field;
+
+        // Read each field based on column headers
+        getline(ss, ufo.city, ',');
+        getline(ss, ufo.state, ',');
+        getline(ss, ufo.country, ',');
+
+        getline(ss, ufo.shape, ',');
+        getline(ss, ufo.duration, ',');
+        getline(ss, ufo.description, ',');
+
+        // removes commas so lat/long can be converted to double
+        getline(ss, field, ',');
+        ufo.latitude = stod(field.substr(1, field.size() - 2));
+        getline(ss, field, ',');
+        ufo.longitude = stod(field.substr(1, field.size() - 2));
+
+        getline(ss, ufo.year, ',');
+        getline(ss, ufo.month, ',');
+        getline(ss, ufo.day, ',');
+        getline(ss, ufo.hour, ',');
+        getline(ss, ufo.minute, ',');
+
+        getline(ss, ufo.year_doc, ',');
+        getline(ss, ufo.month_doc, ',');
+        getline(ss, ufo.day_doc, ',');
+
+        ufoList.push_back(ufo);
+    }
+
+    cout << ufoList[0].latitude << endl;
+
 }
+
+
